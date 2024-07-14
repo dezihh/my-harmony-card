2416a55834a787de5a55cecb15542087d4832724
Derek Ziegler <derek@web.de>
2024-07-14 15:21:13 +0000

    versioning

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,5 @@
 export const CARD_VERSION = "v@MY_HARMOY_CARD_VERSION_PLACEHOLDER@";
 export const CARD_TAG_NAME = "my-harmony-card";
 export const EDITOR_CARD_TAG_NAME = "my-harmony-card-editor";
+
+//
