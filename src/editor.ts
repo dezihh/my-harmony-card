46f1ad17d8cb3edb4a2d63db549233e3063cde0a
dezihh <44939692+dezihh@users.noreply.github.com>
2024-08-05 12:11:33 +0200

    Update editor.ts
    Representation revised and view in the editor cleaned up.
    Activities part completely revised. 
    The activities are now automatically adopted when you press the sync button.
    Incorrect/unnecessary activities are removed by pressing the sync button.
    Existing activities that exist will not be modified.

--- src/editor.ts
+++ src/editor.ts
@@ -4,14 +4,14 @@ import { html, css, LitElement } from "lit";
 
 import { HomeAssistantFixed } from "./types";
 import { EDITOR_CARD_TAG_NAME } from "./const";
+import { styles } from './styles'; // Importing styles
 
 interface ActivityData {
-  //name: string;
   device_id: number;
 }
 
 @customElement(EDITOR_CARD_TAG_NAME)
-class LgRemoteControlEditor extends LitElement {
+class MYHarmonyCardEditor extends LitElement {
   private _config: any;
   private hass: HomeAssistantFixed;
 
@@ -25,45 +25,13 @@ class LgRemoteControlEditor extends LitElement {
   setConfig(config) {
     this._config = config;
   }
-  //  if (!this._config.activities || !this._config.activities.PowerOff) {
-  //    this._config.activities = {
-  //      ...this._config.activities,
-  //      PowerOff: {
-  //        name: 'Poweroff',
-  //        device_id: -1
-  //      }
-  //    };
-  //  }
-  //}
 
-
-
-  //###############
   configChanged(ev) {
-
     const _config = Object.assign({}, this._config);
     _config[ev.target.name.toString()] = ev.target.value;
     this._config = _config;
 
-    // A config-changed event will tell lovelace we have made changed to the configuration
-    // this make sure the changes are saved correctly later and will update the preview
     const event = new CustomEvent("config-changed", {
-      detail: { config: _config },
-      bubbles: true,
-      composed: true,
-    });
-    this.dispatchEvent(event);
-  }
-
-  configChangedBool(ev) {
-    const inputName = ev.target.name;
-    const newValue = ev.target.value === 'true';
-
-    const _config = { ...this._config };
-    _config[inputName] = newValue;
-    this._config = _config;
-
-    const event = new CustomEvent('config-changed', {
       detail: { config: _config },
       bubbles: true,
       composed: true,
@@ -118,12 +86,11 @@ class LgRemoteControlEditor extends LitElement {
     this.dispatchEvent(event);
   }
 
-  getLgTvEntityDropdown(optionValue) {
+  getHarmonyEntityDropdown(optionValue) {
     let remoteEntities = Object.keys(this.hass.states).filter(e => e.startsWith('remote.'));
     let heading = 'Harmony Remote Entity';
     let blankEntity = html``;
 
-    //if (this._config.tventity == '' || !(remoteEntities).includes(optionValue)) {
     if (this._config.entity == '' || !(remoteEntities).includes(optionValue)) {
       blankEntity = html`<option value="" selected> - - - - </option>`;
     }
@@ -142,16 +109,15 @@ class LgRemoteControlEditor extends LitElement {
           }
         })}
       </select>
-      <br>
-      <br>
+      <br><br>
     `;
   }
 
   setRemoteName(remoteNameValue) {
-    let heading = 'Remote Control Name (option):';
+    let heading = 'Remote Control Name (optional):';
     return html`
       ${heading}<br>
-      <input type="text" name="name" id="remote-name" style="width: 37.8ch; padding: .6em; font-size: 1em;" .value="${remoteNameValue}"
+      <input type="text" name="name" id="remote-name" style="width: 37.8ch; padding: .6em; font-size: 1em;" .value="${remoteNameValue || ''}"
              @input=${this.configChanged}>
       <br><br>
     `;
@@ -165,107 +131,166 @@ class LgRemoteControlEditor extends LitElement {
     }
 
     return html`
-      <label class="color-item" for="text">Text Color:</label>
-      <input type="color" name="text" id="text"  .value="${config.colors && config.colors.text || ''}"
-             @input=${this.colorsConfigChanged}></input>
-      <ha-icon data-input-name="text" icon="mdi:trash-can-outline" @click=${this.colorsConfigChanged}></ha-icon>
-
-      <label class="color-item" for="background">Background Color:</label>
-      <input type="color" name="background" id="background"  .value="${config.colors && config.colors.background || ''}"
-             @input=${this.colorsConfigChanged}></input>
-      <ha-icon data-input-name="background" icon="mdi:trash-can-outline" @click=${this.colorsConfigChanged}></ha-icon>
-
-      <label class="color-item" for="border">Border Color:</label>
-      <input type="color" name="border" id="border"  .value="${config.colors && config.colors.border || ''}"
-             @input=${this.colorsConfigChanged}></input>
-      <ha-icon data-input-name="border" icon="mdi:trash-can-outline" @click=${this.colorsConfigChanged}></ha-icon>
+      <hr style="width: 100%; height: 1px; border: none; border-top: 1px solid #eeeeee;">
+      <div class="heading">Colors</div>
+      <div>
+        <input type="color" name="background" id="background" .value="${config.colors.background || ''}"
+               @input=${this.colorsConfigChanged}>
+        <ha-icon data-input-name="background" icon="mdi:trash-can-outline" @click=${this.colorsConfigChanged}></ha-icon>
+        <label class="color-item" for="background">Background Color</label>
+      </div>
+      <div>
+        <input type="color" name="text" id="text" .value="${config.colors.text || ''}"
+               @input=${this.colorsConfigChanged}>
+        <ha-icon data-input-name="text" icon="mdi:trash-can-outline" @click=${this.colorsConfigChanged}></ha-icon>
+        <label class="color-item" for="text">Text Color</label>
+      </div>
+      <div>
+        <input type="color" name="border" id="border" .value="${config.colors.border || ''}"
+               @input=${this.colorsConfigChanged}>
+        <ha-icon data-input-name="border" icon="mdi:trash-can-outline" @click=${this.colorsConfigChanged}></ha-icon>
+        <label class="color-item" for="border">Border Color</label>
+      </div>
     `;
   }
 
   setDimensions(dimensions) {
     let heading = 'Dimensions';
+    const defaultDimensions = {
+      scale: 1,
+      border_width: 1
+    };
 
-    const borderWidth = parseFloat(dimensions.border_width ?? "1");
+    const actualDimensions = {
+      ...defaultDimensions,
+      ...(dimensions || {})
+    };
+
+    const borderWidth = parseFloat(actualDimensions.border_width);
 
     return html`
-      <div class="heading">${heading}:</div>
+      <hr style="width: 100%; height: 1px; border: none; border-top: 1px solid #eeeeee;">
+      <div class="heading" style="margin-bottom: -10px;">${heading}:</div>
       <br>
-      <label for="scale">Card Scale: ${dimensions.scale ?? 1}</label><br>
-      <input type="range" min="0.5" max="1.5" step="0.01" .value="${dimensions && dimensions.scale}" id="scale" name="scale" @input=${this.dimensionsConfigChanged} style="width: 40ch;">
-      </input>
-      <br>
-      <br>
+      <label for="scale" style="margin-top:0px;">Card Scale: ${actualDimensions.scale}</label><br>
+      <input type="range" min="0.5" max="1.5" step="0.01" .value="${actualDimensions.scale}" id="scale" name="scale" @input=${this.dimensionsConfigChanged} style="width: 40ch;">
+      <br><br>
       <label for="border_width">Card Border Width: ${borderWidth}px</label><br>
       <input type="range" min="1" max="5" step="1" .value="${borderWidth}" id="border_width" name="border_width" @input=${this.dimensionsConfigChanged} style="width: 40ch;">
-      </input>
       <br>
     `;
   }
 
-  renderActivities(activities){
-    const stateObj = this.hass.states[this._config.entity];
-    const activityList = stateObj.attributes.activity_list;
-  
-    // Get the keys of the activities in the config
-    const configActivityKeys = Object.keys(this._config.activities);
-  
-    // Add new activities from activityList that are not in configActivityKeys
-    const newActivities = activityList.filter(activity => !configActivityKeys.includes(activity));
-    const toDeleteActivities = configActivityKeys.filter(key => !activityList.includes(key));
-    const commonActivities = configActivityKeys.filter(key => activityList.includes(key));
-    
-    console.log("Gemeinsame Ojekte", commonActivities);
-    console.log('New activities:', newActivities);
-    console.log('toDelete', toDeleteActivities);
-
-    commonActivities.forEach(key => {
-      console.log(`Device ID for ${key}: ${this._config.activities[key]?.device_id}`);
-    });
-
-    console.log(JSON.stringify(configActivityKeys, null, 2));
-    console.log(JSON.stringify(activityList, null, 2));
-  
+  importActivities() {
     return html`
-      <div class="heading">Activities:</div>
-      <br>
-      ${commonActivities.map((activityData, index) => html`
-        <div class="activity-container">
-          <div class="form-group">
-          <label for="activity-${index}-key" class="activity-identifier">${activityData}:</label>
-  
-          <div class="form-group">
-            <label for="activity-${index}-device_id">Device ID:</label>
-          <input type="number" id="activity-id" name="${activityData}.device_id" .value="${this._config.activities[activityData]?.device_id}" title="The number of the primary device for this activity (in harmony_*.conf)" @input="${this.configChanged}" class="activity-input">
+      <hr style="width: 100%; height: 1px; border: none; border-top: 1px solid #eeeeee;">
+      <div class="import-activities">
+        <input type="checkbox" id="importActivitiesCheckbox" @change=${this.handleImportActivitiesCheckboxChange} />
+        <ha-icon data-input-name="text" icon="mdi:chevron-left"></ha-icon>
+        <label for="importActivitiesCheckbox">Synchronize Activities from Harmony</label>
+        <div style="margin-left: 20px">
+          <i>Use this for initialization of known activities in Home Assistant.<br>
+          Hint: You still have to fill in the device_id element for each activity below!</i>
         </div>
-      `)}
-      <button @click="${this.addActivity}" class="add-activity-button">Add New Activity</button>
+      </div>
+      <hr style="width: 100%; height: 1px; border: none; border-top: 1px solid #eeeeee;">
     `;
   }
 
-  addActivity() {
-    const _config = { ...this._config };
-    const newActivityName = `Activity${Object.keys(_config.activities).length}`;
-    _config.activities[newActivityName] = {
-      name: '',
-      device_id: ''
-    };
-    this._config = _config;
-
-    const event = new CustomEvent("config-changed", {
-      detail: { config: _config },
-      bubbles: true,
-      composed: true,
-    });
-    this.dispatchEvent(event);
+  handleImportActivitiesCheckboxChange(event) {
+    const checkbox = event.target;
+    if (event.target.checked) {
+      this.syncActivities();
+    }
+    setTimeout(() => {
+      checkbox.checked = false;
+    }, 200);
   }
 
-  removeActivity(activityName) {
-    const _config = { ...this._config };
-    delete _config.activities[activityName];
-    this._config = _config;
+  checkActivities() {
+    const configActivities = this._config.activities || {};
+    if (typeof configActivities !== 'object' || configActivities === null) {
+      return html`
+        <div class="import-activities">
+          <label for="checkActivitiesNo">"No activities configured, yet"</label>
+        </div>
+      `;
+    }
+
+    const activityKeys = Object.keys(configActivities);
+    if (activityKeys.length > 0) {
+      return html`
+        <div class="heading" style="margin-bottom: -10px;">Activities:</div>
+        <br>
+        <div class="outer-container">
+          ${activityKeys.map((key, index) => {
+            let activityData = { ...configActivities[key] };
+            if (!activityData.device_id) {
+              activityData.device_id = '-1';
+              const updatedConfig = { ...this._config.activities, [key]: activityData };
+              this._config = { ...this._config, activities: updatedConfig };
+              this.requestUpdate();
+            }
+
+            return html`
+              <div class="activity-container">
+                <div class="form-group">
+                  <label for="activity-${index}-key" class="activity-identifier">${key}:</label>
+                  <input type="text" id="activity-${index}-device-id" .value="${activityData.device_id}"
+                    @input="${e => this._updateDeviceId(key, e.target.value)}"
+                    maxlength="9"
+                    class="activity-input">
+                </div>
+              </div>
+            `;
+          })}
+        </div>
+      `;
+    } else {
+      return html`
+        <div class="import-activities">
+          <label for="checkActivitiesNo">"No activities configured, yet"</label>
+        </div>
+      `;
+    }
+  }
+
+  _updateDeviceId(key, value) {
+    const updatedActivity = { ...this._config.activities[key], device_id: value };
+    const updatedActivities = { ...this._config.activities, [key]: updatedActivity };
+    this._config = { ...this._config, activities: updatedActivities };
+    this.dispatchEvent(new CustomEvent('config-changed', { detail: { config: this._config } }));
+  }
+
+  syncActivities() {
+    const stateObj = this.hass.states[this._config.entity];
+    const activityList = stateObj.attributes.activity_list || [];
+
+    const configCopy = { ...this._config };
+
+    if (!configCopy.activities) {
+      configCopy.activities = {};
+    } else {
+      configCopy.activities = { ...configCopy.activities };
+    }
+
+    const configActivityKeys = Object.keys(configCopy.activities);
+    const newActivities = activityList.filter(activity => !configActivityKeys.includes(activity));
+    newActivities.forEach(key => {
+      configCopy.activities[key] = {
+        device_id: -1
+      };
+    });
+
+    const toDeleteActivities = configActivityKeys.filter(key => !activityList.includes(key));
+    toDeleteActivities.forEach(key => {
+      delete configCopy.activities[key];
+    });
+
+    this._config = configCopy;
 
     const event = new CustomEvent("config-changed", {
-      detail: { config: _config },
+      detail: { config: this._config },
       bubbles: true,
       composed: true,
     });
@@ -273,92 +298,40 @@ class LgRemoteControlEditor extends LitElement {
   }
 
   render() {
-   if (!this.hass || !this._config) {
-     return html``;
-   }
+    if (!this.hass || !this._config) {
+      return html``;
+    }
 
-   return html`
-     <div class="card-config">
-       ${this.getLgTvEntityDropdown(this._config.entity)}
-       ${this.setRemoteName(this._config.name)}
-       ${this.selectColors(this._config)}
-       ${this.setDimensions(this._config.dimensions)}
-       ${this.renderActivities(this._config.activities)}
-      <br>
-      <p>At least the following entries must be present for the card to work:</p>
-      <pre>
-      - type: &#39;custom:my-harmony-card&#39;
-        entity: remote.&lt;device&gt;
-        activities:
-          PowerOff:
-            device_id: -1 </pre>
-      <p>&nbsp;</p>
-      <p><br />
-      For more options please visit <a href="https://github.com/dezihh/my-harmony-card" target="_blank">https://github.com/dezihh/my-harmony-card</a></p>
-
-      <div class="donations" style="display: flex">
-          <a href="https://buymeacoffee.com/dezi" target="_blank">
-          <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
-          alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;" ></a>
+    return html`
+      <div class="card-config">
+        ${this.getHarmonyEntityDropdown(this._config.entity)}
+        ${this.setRemoteName(this._config.name)}
+        ${this.selectColors(this._config)}
+        ${this.setDimensions(this._config.dimensions)}
+        ${this.importActivities()}
+        ${this.checkActivities()}
+        <br>
+        <div class="info">
+          <div class="heading">Additional Info:</div>
+          <div>
+            <u>Hint:</u> Please find the number of each configured activity in the root directory of Home Assistant in the file <b><~/harmony_??????.conf></b>.<br>
+            Each device listed in there has its own ID, like <i><"id": "77085993"></i>. Take this number for each device listed above and fill it in there.<br>
+          </div>
+          <hr style="width: 100%; height: 1px; border: none; border-top: 1px solid #eeeeee;">
+          For more options please visit <a href="https://github.com/dezihh/my-harmony-card" target="_blank">https://github.com/dezihh/my-harmony-card</a>.
+          <div class="donations" style="display: flex">
+            <a href="https://buymeacoffee.com/dezi" target="_blank">
+              <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+              alt="Buy Me A Coffee" style="height: 60px !important;width: 217px !important;">
+            </a>
+          </div>
+        </div>
       </div>
-   `;
+    `;
   }
 
-static get styles() {
-  return css`
-    .form-group {
-      margin-bottom: 16px;
-    }
-
-    .form-group label {
-      display: block;
-      margin-bottom: 4px;
-      font-weight: bold;
-    }
-
-    .form-group input[type="number"],
-    .form-group input[type="number"] {
-      width: 20ch; /* Set width to 20 characters */
-      padding: 8px;
-      box-sizing: border-box;
-    }
-
-    .activity-container {
-      margin-bottom: 16px;
-      padding: 16px;
-      border: 1px solid #ccc;
-      border-radius: 8px;
-      filter: brightness(0.9); /* Slightly darker */
-    }
-
-    .activity-identifier {
-      font-weight: bold;
-      width: 150px; /* Adjust the width as necessary */
-    }
-
-    .activity-input {
-      margin-left: 8px;
-      width: 20ch; /* Set width to 20 characters */
-    }
-
-    .remove-button {
-      margin-left: auto;
-    }
-
-    .activity-details {
-      padding-left: 26px;
-      margin-top: 8px;
-    }
-
-    .add-activity-button {
-      margin-top: 16px;
-    }
-    
-    .heading {
-       font-weight: bold;
-    }
-  `;
+  static get styles() {
+    return styles;
   }
-  
-
 }
+
