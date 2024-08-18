054a8c7ec6b1c340b024015e86e72c4cafe304b1
dezihh <44939692+dezihh@users.noreply.github.com>
2024-08-18 18:59:50 +0200

    Update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,6 +5,7 @@ import babel from "@rollup/plugin-babel";
 import terser from "@rollup/plugin-terser";
 import serve from "rollup-plugin-serve";
 import json from "@rollup/plugin-json";
+import copy from 'rollup-plugin-copy';
 
 const dev = process.env.ROLLUP_WATCH;
 
@@ -24,12 +25,19 @@ const plugins = [
     typescript(),
     json(),
     babel({
-        exclude: "node_modules/**",
-        babelHelpers: 'bundled' // Explicitly setting babelHelpers option
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**', // Schließt node_modules von der Babel-Transpilation aus
+      presets: [
+        ['@babel/preset-env', { targets: "defaults" }]
+      ],
+      plugins: [
+        '@babel/plugin-transform-runtime',
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-proposal-decorators',
+      ]
     }),
-    dev && serve(serveopts),
-    !dev && terser(),
-];
+    terser() // optional, minimiert das Bundle
+  ];
 
 export default [
     {
