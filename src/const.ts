8c904d9591427c07289de3afa2ca24e92eea3c2b
Derek <dev@ziegler.zip>
2024-09-09 08:35:42 +0000

    v1.30.2

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,4 @@
-export const CARD_VERSION = "v@MY_HARMOY_CARD_VERSION_PLACEHOLDER@";
+export const CARD_VERSION = "@MY_HARMONY_CARD_PLACEHOLDER@";
 export const CARD_TAG_NAME = "my-harmony-card";
 export const EDITOR_CARD_TAG_NAME = "my-harmony-card-editor";
 
